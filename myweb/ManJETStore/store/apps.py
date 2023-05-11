24010f8894ea2fc4a084dc86926222dfd14cfb75
simon <lethaitrungtin@gmail.com>
2023-05-11 09:16:55 +0700

    first commit

---
+++ myweb/ManJETStore/store/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+class StoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'store'
